0c7406264e0e91923481f9dca421e4f08014db6b
Andre <awjrs22@gmail.com>
2019-10-02 16:57:17 -0700

    Adds MongoDB and playlist model

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
-
-
+client = MongoClient()
+db = client.Movie
+movies = db.movies
+'''
+movies = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+'''
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def movies_index():
+def index():
     """return homepage"""
-    return render_template('index.html')
+    return render_template('home.html')
 
 
-
+@app.route('/movies')
+def movies_index():
+    """Show all movies."""
+    return render_template('movies_index.html', movies=movies)
 
 
 

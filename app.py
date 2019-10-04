e411fadebc358029019197edacf00e91745526d7
Andre <awjrs22@gmail.com>
2019-10-04 14:30:09 -0700

    cleans code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def movies_index():
     """Show all movies."""
-    return render_template('movies_index.html', movies=movies.find())
+    return render_template('movies_new.html', movie={},title='New_Movie')
 
 @app.route('/movies/new')
 def movie_new():
@@ -37,7 +37,24 @@ def movies_show(movie_id):
     movie = movies.find_one({'_id': ObjectId(movie_id)})
     return render_template('movies_show.html', movie=movie)
 
+@app.route('/movies/<movie_id>/edit')
+def movies_edit(movie_id):
+    """Show the edit form for a playlist."""
+    movie = movies.find_one({'_id': ObjectId(movie_id)})
+    return render_template('movies_edit.html', movie=movie, title='Edit Movie')
 
+@app.route('/movies/<movie_id>', methods=['POST'])
+def movies_update(movie_id):
+    """Submit an edited playlist."""
+    updated_movie = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    movies.update_one(
+        {'_id': ObjectId(movie_id)},
+        {'$set': updated_movie})
+    return redirect(url_for('movies_show', movie_id=movie_id))
 
 if __name__ == '__main__':
     app.run(debug=True)

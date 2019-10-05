b9c3172cfe60c3528f030a2314382b6df4eb7286
Andre <awjrs22@gmail.com>
2019-10-05 09:30:04 -0700

    adding Procfile

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
-client = MongoClient()
-db = client.Movie
+import os
+import requests
+import json
+#import vimeo
+host = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/Movie')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 movies = db.movies
 
+MOVIE_API_KEY = os.getenv("MOVIE_API_KEY")
 
 app = Flask(__name__)
 
@@ -13,7 +18,30 @@ app = Flask(__name__)
 @app.route('/')
 def movies_index():
     """Show all movies."""
-    return render_template('movies_index.html', movies=movies.find())
+    results = home_random()
+    #list_of_movies = results['results']
+    print(results)
+
+    return render_template('movies_index.html', movies=movies.find(), list_of_movies=results)
+
+def home_random():
+
+    
+    response = requests.get('https://api.themoviedb.org/3/trending/movie/day?api_key=78051a9c38f50943f38976f234aeba7b')
+    if response.status_code == 200:
+        random_movies = json.loads(response.content)
+    else:
+        random_movies = None
+
+    '''  client = vimeo.VimeoClient(
+    token='dd537c6ac1efaf0e1280d36d47f69e89',
+    key='{client_id}',
+    secret='{client_secret}'
+    )
+    response = client.get(uri)
+    print(response.json())'''
+    return random_movies
+
 
 @app.route('/movies/new')
 def movie_new():
@@ -50,6 +78,7 @@ def movies_update(movie_id):
     updated_movie = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
+        'link': request.form.get('link'),
         'videos': request.form.get('videos').split()
     }
     movies.update_one(
@@ -65,4 +94,4 @@ def movies_delete(movie_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+ app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
